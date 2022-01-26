b81aa37c299d0dca9919180c8bce75bd33de6df7
iantsmyth <iansmyth95@gmail.com>
2022-01-26 01:31:49 -0500

    Initial commit

---
+++ apple_trivia.py
@@ -0,0 +1,14 @@
+question_1 = input("What year was Apple founded? ")
+while int(question_1) != 1976:
+	question_1 = input("Try again: What year was Apple founded? ")
+print("Correct!")
+
+question_2 = input("What breakthrough product was released in 2001? ")
+while question_2 != "iPod":
+  question_2 = input("Try again: What breakthrough product was released in 2001? ")
+print("Correct!")
+
+question_3 = input("Where did Steve Jobs travel to when he was 19? ")
+while question_3 != "India":
+	question_3 = input("Try again: Where did Steve Jobs travel to when he was 19? ")
+print("Correct!")
